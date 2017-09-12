b3d22fb072610ab50fcd3cb06c1c09cf5236c7f6
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-09-12 17:53:37 -0400

    models-products & models-category #1 #5

---
+++ db/models/products.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+
+const db = require('../_db');
+const Category = db.models.Category;
+
+const Product = db.define('product', {
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  imageName: {
+    type: Sequelize.TEXT,
+    defaultValue: 'default_product_img.png'
+  }
+}, {
+  // validate: {
+  //   hasCategories() {
+  //     // get the categories asynch
+  //     if (this.getCategories().length===0) throw new Error('Require at least one category per item')
+  //   }
+  // },
+  defaultScope: {
+    include: [{ model: Category }]
+  }
+});
+
+module.exports = Product;
