1d00f2be69ef0d2ea9662b989f1dc9a6214cf111
Li Wang <esmbug@gmail.com>
2017-09-13 14:36:20 -0400

    copy seed file

--- server/start.js
+++ server/start.js
@@ -35,6 +35,7 @@ app.use(require('./passport.middleware'));
 
 
 app.use('/api', require('./api'));
+app.use('/products', require('./products'));
 app.get('/*', (_, res) => { res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))});
 
 app.use(function (err, req, res, next) {
