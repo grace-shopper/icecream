7971794b675700cd3e29a2e942eec1f69c7f17d4
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-09-13 22:45:24 -0400

    updated navbar to include products and your cart and your profile, closing #48

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,48 +1,55 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import AppBar from 'material-ui/AppBar'; 
-import Drawer from 'material-ui/Drawer'; 
-import MenuItem from 'material-ui/MenuItem'; 
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 export default class Navbar extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isLoggedIn : false, 
+			isLoggedIn: false,
 			open: false
 		}
-		this.handleClose = this.handleClose.bind(this); 
-		this.handleToggle = this.handleToggle.bind(this); 
+		this.handleClose = this.handleClose.bind(this);
+		this.handleToggle = this.handleToggle.bind(this);
 	}
 
 	handleToggle(e) {
-		e.preventDefault(); 
-		this.setState({ open: !this.state.open }); 
+		e.preventDefault();
+		this.setState({ open: !this.state.open });
 	}
 
 	handleClose(e) {
-		this.setState({ open: false }); 
+		this.setState({ open: false });
 	}
 
 	render() {
 		return (
-			<div> 
-				<AppBar 
-					title="Gracey Hopper's Ice Screamatorium" 
-					onLeftIconButtonTouchTap={this.handleToggle}/>
+			<div>
+				<AppBar
+					title="Gracey Hopper's Ice Screamatorium"
+					onLeftIconButtonTouchTap={this.handleToggle} />
 				<Drawer docked={false}
 					width={200}
 					open={this.state.open}
-					onRequestChange={(open) => this.setState({open})}>
-				<MenuItem onClick={this.handleClose}><Link to="/">Login</Link></MenuItem>
-				<MenuItem onClick={this.handleClose}><Link to="/">Sign Up</Link></MenuItem>
-				{
-					(this.state.isLoggedIn) 
-					? <MenuItem onClick={this.handleClose}><Link to="/">Logout</Link></MenuItem>
-					: null
-				}
-				</Drawer> 
-			</div> 
+					onRequestChange={(open) => this.setState({ open })}>
+					<MenuItem onClick={this.handleClose}><Link to="/">Login</Link></MenuItem>
+					<MenuItem onClick={this.handleClose}><Link to="/">Sign Up</Link></MenuItem>
+					{
+						(this.state.isLoggedIn)
+							? <MenuItem onClick={this.handleClose}><Link to="/">Logout</Link></MenuItem>
+							: null
+					}
+					<MenuItem onClick={this.handleClose}><Link to="/products">Products</Link></MenuItem>
+					<MenuItem onClick={this.handleClose}><Link to="/cart">Your Cart</Link></MenuItem>
+					{
+						(this.state.isLoggedIn)
+							? <MenuItem onClick={this.handleClose}><Link to="/">Your Profile</Link></MenuItem>
+							: null
+					}
+				</Drawer>
+			</div>
 		)
 	}
 }
--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -34,7 +34,7 @@ export default class Root extends Component {
 						<Route path="/cart" component={Cart}/>
 						<Route exact path="/" component={AllProducts}/>
 						<Route exact path="/products" component={AllProducts}/>
-						<Route path="/products/:productId" component={SingleProduct}/>
+						<Route exact path="/products/:productId" component={SingleProduct}/>
 					</Switch>
 				</div>
 			</div>
