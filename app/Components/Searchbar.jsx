7eb137546362c2913c073729c765cd663eaa03df
corvids <joonnifer@gmail.com>
2017-09-14 12:16:53 -0400

    start searchbar component using material ui

---
+++ app/Components/Searchbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import AutoComplete from 'material-ui/AutoComplete';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import {fetchProducts, getProduct} from '../reducers';
+
+export class Searchbar extends Component {
+
+  render() {
+    return (
+
+    );
+  };
+
+}
