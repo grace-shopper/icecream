a34acd5838677838ee05004c9dcc1ccb4636b39f
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-09-18 16:45:04 -0400

    fixes refresh issue on cart page

--- server/cart.middleware.js
+++ server/cart.middleware.js
@@ -2,22 +2,33 @@ const router = require('express').Router();
 const Order = require('../db/models/orders');
 // store quantity of cart
 router.use((req, res, next) => {
+
+  // if there's not a cartId on the session yet...
   if (!req.session.cartId) {
     if (req.user) {
-      console.log('user', req.user)
       Order.findOne({
         where: {
           userId: req.user.id,
           status: "In Cart"
         }
       })
-      // a user logins in but doesn't have a cart, nothing is returned
       .then(cart => {
         req.cart = cart
         req.session.cartId = cart.id
       })
+      .catch(next)
     }
   }
+
+  // if there is a session id
+  else {
+    Order.findById(req.session.cartId)
+    .then(cart => {
+      req.cart = cart;
+    })
+    .catch(next)
+  }
+
   next()
 })
 
