62bb7de004143d4b8d9276ef631dbc2bde0d59c7
corvids <joonnifer@gmail.com>
2017-09-18 17:06:41 -0400

    removed console logs from routes

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -22,7 +22,6 @@ router.get('/:productId', (req, res, next) => {
 
 // create a new review for a product
 router.post('/', (req, res, next) => {
-  console.log(' in the reviews route ', req.body)
 	Review.create(req.body)
 	.then(review => res.json(review))
   .catch(next);
