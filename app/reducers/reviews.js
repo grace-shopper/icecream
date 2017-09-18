8853dd73f3d3a556325770846948f92f81f3515b
corvids <joonnifer@gmail.com>
2017-09-18 11:41:58 -0400

    add thunk and routes for posting a review

--- app/reducers/reviews.js
+++ app/reducers/reviews.js
@@ -2,10 +2,16 @@ import axios from 'axios';
 
 // ACTION TYPES
 const GET_REVIEWS = 'GET_REVIEWS';
+const ADD_REVIEW = 'ADD_REVIEW';
 
 // ACTION CREATORS
 export function getReviews(reviews) {
   const action = { type: GET_REVIEWS, reviews };
+  return action;
+}
+
+export function addReview(review) {
+  const action = { type: ADD_REVIEW, review };
   return action;
 }
 
@@ -20,11 +26,23 @@ export function fetchReviews(id) {
   }
 }
 
+export function postReview(review) {
+  return function thunk(dispatch) {
+    return axios.post(`/api/reviews`, review)
+      .then(res => res.data)
+      .then(review => {
+        dispatch(addReview(review));
+      });
+  }
+}
+
 // reducer
 const reducer = function (state = [], action) {
   switch (action.type) {
     case GET_REVIEWS:
       return action.reviews
+    case ADD_REVIEW:
+      return [...state, action.review]
     default:
       return state
   }
--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const Review = require('../../db/models/reviews');
 const db = require('../../db').db;
 
+// get all reviews
 router.get('/', (req, res, next) => {
   Review.findAll()
     .then( reviews => res.json(reviews) )
     .catch(next);
 });
 
+// get reviews by product id
 router.get('/:productId', (req, res, next) => {
   Review.findAll({
     where: {
@@ -18,5 +20,10 @@ router.get('/:productId', (req, res, next) => {
   .catch(next);
 })
 
+// create a new review for a product
+router.post('/', (req, res, next) => {
+	Review.create(req.body)
+	.then(product => res.json(review));
+});
 
 module.exports = router;
